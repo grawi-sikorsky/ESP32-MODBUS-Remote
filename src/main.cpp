74c2a8ed03798217d50b53a54a309d6f971dbd19
JobLaptok <grawires@gmail.com>
2022-08-08 09:33:01 +0200

    BT

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include "WiFi.h"
+#include "WiFiManager.h"
+#include "BluetoothSerial.h"
 #include "HTTPClient.h"
 #include <ArduinoJson.h>
 
@@ -7,17 +9,26 @@
 // #define WIFI_NAME "Naplet"
 // #define WIFI_PASS "Napletszatana89"
 #define WIFI_NAME "PrzekleteObliczeBelzebuba"
-#define WIFI_PASS "xx"
+#define WIFI_PASS "Lilicapirowek89"
 #define WIFI_DEVICE_NAME "ESP32 Divajs"
 #define WIFI_TIMEOUT_MS 20000
 
 #define POST_INTERVAL 60000
 
+WiFiManager wifiManager;
+
+BluetoothSerial ESP_BT;
+String bluetoothData;
+std::string wifiName;
+std::string wifiPass;
+std::string espName;
+
 time_t prevTime;
 
 int i = 1;
 
-void connectToWifi(){
+void connectToWifi()
+{
   Serial.println("WIFI Connecting...");
   WiFi.mode(WIFI_STA);
   WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
@@ -26,20 +37,46 @@ void connectToWifi(){
 
   unsigned long startTime = millis();
 
-  while(WiFi.status() != WL_CONNECTED && millis() - startTime < WIFI_TIMEOUT_MS){
+  while (WiFi.status() != WL_CONNECTED && millis() - startTime < WIFI_TIMEOUT_MS)
+  {
     Serial.print(" .");
     delay(1000);
   }
 
-  if(WiFi.status() != WL_CONNECTED){
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("WIFI Connection Failed.. ");
   }
-  else{
+  else
+  {
     Serial.println("\nWIFI Connection is established.");
   }
 }
 
-void sendPost(int someint){
+void setupBluetooth()
+{
+  ESP_BT.begin("ESP32-Modbus");
+}
+
+void setWifiCredentialsFromBT(){
+  std::string delimiter = "\n";
+  std::string costam = std::string(bluetoothData.c_str());
+  const char* temp;
+  Serial.println(costam.c_str());
+  temp = costam.c_str();
+
+  wifiName = costam.substr(0, costam.find("\n"));
+  wifiPass = costam.substr(wifiName.length(), costam.find(delimiter));
+  espName = costam.substr(wifiPass.length(), costam.find(delimiter));
+
+
+  Serial.println(wifiName.c_str());
+  Serial.println(wifiPass.c_str());
+  Serial.println(espName.c_str());
+}
+
+void sendPost(int someint)
+{
   HTTPClient http;
   http.begin("https://modbus-back.herokuapp.com/data");
   http.addHeader("Content-Type", "application/json");
@@ -48,23 +85,32 @@ void sendPost(int someint){
   doc["modbusID"] = "modbus1";
   doc["batteryVoltage"] = someint;
   doc["solarVoltage"] = 22;
-    
+
   String requestBody;
   serializeJson(doc, requestBody);
-    
+
   int httpResponseCode = http.POST(requestBody);
- 
+
   http.end();
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
+  setupBluetooth();
   connectToWifi();
 }
 
-void loop() {
+void loop()
+{
+  if (ESP_BT.available())
+  {
+    bluetoothData = ESP_BT.readString();
+    Serial.println(bluetoothData);
+    setWifiCredentialsFromBT();
+  }
 
-  if(millis() - prevTime >= POST_INTERVAL )
+  if (millis() - prevTime >= POST_INTERVAL)
   {
     i++;
     Serial.println(i);
@@ -73,7 +119,8 @@ void loop() {
 
     prevTime = millis();
   }
-  else{
+  else
+  {
     // TODO: Getting data from modbus sensors?
   }
 }
