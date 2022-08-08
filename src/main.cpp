92030bc357d2a32ece7479b6e817000c6afa72e1
JobLaptok <grawires@gmail.com>
2022-08-08 13:38:43 +0200

    bme

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
 #include <Arduino.h>
-// #include "WiFi.h"
 #include "WiFiManager.h"
-#include "BluetoothSerial.h"
 #include "HTTPClient.h"
 #include <ArduinoJson.h>
+#include <Adafruit_BMP280.h>
+#include <Wire.h>
+
+Adafruit_BMP280 bme;
 
 // DEFINES
-// #define WIFI_NAME "Naplet"
-// #define WIFI_PASS "Napletszatana89"
-#define WIFI_NAME "PrzekleteObliczeBelzebuba"
-#define WIFI_PASS "xx"
-#define WIFI_DEVICE_NAME "ESP32 Divajs"
-#define WIFI_TIMEOUT_MS 20000
+#define RESET_WIFI_PIN 19
 
 #define POST_INTERVAL 60000
+#define BME_INTERVAL 5000
 
 WiFiManager wifiManager;
 
-time_t prevTime;
+time_t postPrevTime;
+time_t bmePrevTime;
 
 int i = 1;
 
@@ -40,25 +39,72 @@ void sendPost(int someint)
   http.end();
 }
 
+void checkResetWifiButton(){
+  if(digitalRead(RESET_WIFI_PIN) == LOW){
+    wifiManager.resetSettings();
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
-  wifiManager.autoConnect("ESP32-MODBUS", "password");
+  wifiManager.autoConnect("ESP32-MODBUS");
+  pinMode(RESET_WIFI_PIN, INPUT_PULLUP);
+
+  // Wire.begin();
+  // Wire.setClock(400000);
+
+  if (!bme.begin()) {
+  Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                    "try a different address!"));
+  }
+
+  /* Default settings from datasheet. */
+  bme.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
 }
 
 void loop()
 {
-  if (millis() - prevTime >= POST_INTERVAL)
+  if (millis() - postPrevTime >= POST_INTERVAL)
   {
     i++;
     Serial.println(i);
 
     sendPost(i);
 
-    prevTime = millis();
+    postPrevTime = millis();
+  }
+  else if (millis() - bmePrevTime >= BME_INTERVAL)
+  {
+    if (bme.takeForcedMeasurement()) {
+      // can now print out the new measurements
+      Serial.print(F("Temperature = "));
+      Serial.print(bme.readTemperature());
+      Serial.println(" *C");
+
+      Serial.print(F("Pressure = "));
+      Serial.print(bme.readPressure());
+      Serial.println(" Pa");
+
+      Serial.print(F("Approx altitude = "));
+      Serial.print(bme.readAltitude(1013.25)); /* Adjusted to local forecast! */
+      Serial.println(" m");
+
+      Serial.println();
+    } else {
+      Serial.println("Forced measurement failed!");
+    }
+
+    bmePrevTime = millis();
   }
   else
   {
+    checkResetWifiButton();
     // TODO: Getting data from modbus sensors?
   }
 }
